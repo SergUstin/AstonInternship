ff252023c14f607617305a00a1e460a59974cec8
Serg <s.ustin.92@mail.ru>
2024-02-05 16:37:46 +0300

    add constructors

--- RestCrudOnServlet/src/main/java/com/campany/dto/EmployeeDTO.java
+++ RestCrudOnServlet/src/main/java/com/campany/dto/EmployeeDTO.java
@@ -11,10 +11,31 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@AllArgsConstructor
 public class EmployeeDTO {
     Integer id;
     String fullName;
     BigDecimal salary;
     ManagerDTO managerDTO;
+
+    public EmployeeDTO() {
+    }
+
+    public EmployeeDTO(Integer id, String fullName, BigDecimal salary) {
+        this.id = id;
+        this.fullName = fullName;
+        this.salary = salary;
+    }
+
+    public EmployeeDTO(String fullName, BigDecimal salary, ManagerDTO managerDTO) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.managerDTO = managerDTO;
+    }
+
+    public EmployeeDTO(Integer id, String fullName, BigDecimal salary, ManagerDTO managerDTO) {
+        this.id = id;
+        this.fullName = fullName;
+        this.salary = salary;
+        this.managerDTO = managerDTO;
+    }
 }
--- RestCrudOnServlet/src/main/java/com/campany/dto/ManagerDTO.java
+++ RestCrudOnServlet/src/main/java/com/campany/dto/ManagerDTO.java
@@ -12,10 +12,31 @@ import java.util.List;
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@AllArgsConstructor
 public class ManagerDTO {
     Integer id;
     String fullName;
     BigDecimal salary;
     List<EmployeeDTO> employeeDTOS;
+
+    public ManagerDTO() {
+    }
+
+    public ManagerDTO(Integer id, String fullName, BigDecimal salary) {
+        this.id = id;
+        this.fullName = fullName;
+        this.salary = salary;
+    }
+
+    public ManagerDTO(String fullName, BigDecimal salary, List<EmployeeDTO> employeeDTOS) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.employeeDTOS = employeeDTOS;
+    }
+
+    public ManagerDTO(Integer id, String fullName, BigDecimal salary, List<EmployeeDTO> employeeDTOS) {
+        this.id = id;
+        this.fullName = fullName;
+        this.salary = salary;
+        this.employeeDTOS = employeeDTOS;
+    }
 }
