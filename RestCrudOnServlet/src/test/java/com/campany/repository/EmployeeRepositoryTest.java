f049cf9bb02d91a8881883d3e53784819aca970f
Serg <s.ustin.92@mail.ru>
2024-02-13 17:04:59 +0300

    start test for repository

---
+++ RestCrudOnServlet/src/test/java/com/campany/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.campany.repository;
+
+import com.campany.entity.Employee;
+import com.campany.service.EmployeeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmployeeRepositoryTest {
+}
