33bfd39cd7ff848fee4292ecd6836b9822d67532
Serg <s.ustin.92@mail.ru>
2024-02-13 13:50:47 +0300

    add javadoc

--- RestCrudOnServlet/src/main/java/com/campany/repository/RepositoryMethod.java
+++ RestCrudOnServlet/src/main/java/com/campany/repository/RepositoryMethod.java
@@ -2,10 +2,48 @@ package com.campany.repository;
 
 import java.util.List;
 
+/**
+ * Интерфейс для основных операций хранения и управления сущностями типа T в репозитории.
+ *
+ * @param <T> Тип сущности, с которым работает репозиторий
+ */
 public interface RepositoryMethod<T> {
+    /**
+     * Находит сущность по её идентификатору.
+     *
+     * @param id Идентификатор сущности
+     * @return Объект сущности
+     */
     T findById(Integer id);
+
+    /**
+     * Возвращает список всех сущностей данного типа.
+     *
+     * @return Список всех сущностей
+     */
     List<T> findAll();
+
+    /**
+     * Сохраняет новую сущность.
+     *
+     * @param item Объект новой сущности
+     * @throws ClassNotFoundException если класс не найден
+     */
     void save(T item) throws ClassNotFoundException;
+
+    /**
+     * Обновляет существующую сущность.
+     *
+     * @param item Объект сущности для обновления
+     * @throws ClassNotFoundException если класс не найден
+     */
     void update(T item) throws ClassNotFoundException;
+
+    /**
+     * Удаляет сущность по её идентификатору.
+     *
+     * @param id Идентификатор сущности
+     * @throws ClassNotFoundException если класс не найден
+     */
     void deleteById(Integer id) throws ClassNotFoundException;
 }
--- RestCrudOnServlet/src/main/java/com/campany/service/CrudService.java
+++ RestCrudOnServlet/src/main/java/com/campany/service/CrudService.java
@@ -2,10 +2,52 @@ package com.campany.service;
 
 import java.util.List;
 
+/**
+ * Интерфейс для основных операций CRUD (создание, чтение, обновление, удаление) сущностей типа T.
+ *
+ * @param T сущности, с которым работает сервис
+*/
 public interface CrudService<T> {
+
+    /**
+     * Получает сущность по её идентификатору.
+     *
+     * @param id Идентификатор сущности
+     * @return Объект сущности
+     */
     T getById(Integer id);
+
+    /**
+     * Получает все сущности данного типа.
+     *
+     * @return Список всех сущностей
+     */
     List<T> getAll();
+
+    /**
+     * Создаёт новую сущность.
+     *
+     * @param item Объект новой сущности
+     * @return Созданная сущность
+     * @throws ClassNotFoundException если класс не найден
+     */
     T create(T item) throws ClassNotFoundException;
+
+    /**
+     * Обновляет сущность с указанным идентификатором.
+     *
+     * @param id Идентификатор сущности
+     * @param item Объект сущности для обновления
+     * @return Обновлённая сущность
+     * @throws ClassNotFoundException если класс не найден
+     */
     T update(Integer id, T item) throws ClassNotFoundException;
+
+    /**
+     * Удаляет сущность по её идентификатору.
+     *
+     * @param id Идентификатор сущности
+     * @throws ClassNotFoundException если класс не найден
+     */
     void deleteById(Integer id) throws ClassNotFoundException;
 }
